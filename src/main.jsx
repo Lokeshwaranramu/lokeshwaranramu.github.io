15c43cefa9f30bf46c4cebd238364ac2895e55ba
Lokeshwaran Ramu <lokeshramu16@gmail.com>
2026-04-21 15:27:00 +0930

    Fix Safari cache: unregister old service worker, add SW kill-switch

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './App.css'
 
+// Unregister any old service workers and clear stale caches
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister())
+  })
+  caches.keys().then(keys => keys.forEach(key => caches.delete(key)))
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
